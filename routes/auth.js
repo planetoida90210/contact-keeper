a5ab63fdc5a9f09903c962e76a6f6fb6cb2b6143
Michal Bryk <galeriakoloru90210@gmail.com>
2020-02-19 19:29:25 +0100

    creeate auth middleware to protect private routes , create authorization for users (with account)

--- routes/auth.js
+++ routes/auth.js
@@ -3,22 +3,27 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("config");
+const auth = require("../middleware/auth");
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
 
-// @route    GET api/auth
-// @description   Get logged in user
+// @route     GET api/auth
+// @desc      Get logged in user
 // @access    Private
-
-router.get("/", (req, res) => {
-  res.send("Get logged in user");
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
-// @route    POST api/auth
-// @description   Auth user & get token
+// @route     POST api/auth
+// @desc      Auth user & get token
 // @access    Public
-
 router.post(
   "/",
   [
