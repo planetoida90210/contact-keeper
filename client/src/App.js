297bd64a89ccff3ac597579a9d3ab4c07bb3fc0f
Michal Bryk <galeriakoloru90210@gmail.com>
2020-03-01 19:54:19 +0100

    add alers component to layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
+import Alerts from "./components/layout/Alerts";
 import About from "./components/pages/About";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
@@ -20,6 +21,7 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className='container'>
+                <Alerts />
                 <Switch>
                   <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
