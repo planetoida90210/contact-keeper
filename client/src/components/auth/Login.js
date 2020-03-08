e27fff73ca2b7aff4e68ec9430d7e316516f455d
Michal Bryk <galeriakoloru90210@gmail.com>
2020-03-08 15:15:32 +0100

    add login with errors handle and authetication

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
+const Login = props => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
 
-const Login = () => {
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+    if (error === "Invalid Credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    //eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: "",
     password: ""
   });
 
-  const { name, email, password, password2 } = user;
+  const { email, password } = user;
 
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log("Login submit");
+    if (email === "" || password === "") {
+      setAlert("Please fill all fields", "danger");
+    } else {
+      login({
+        email,
+        password
+      });
+    }
   };
   return (
     <div className='form-container'>
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -60,6 +60,28 @@ const AuthState = props => {
     }
   };
   // Login User
+  const login = async formData => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.post("/api/auth", formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  };
 
   // Logout
 
@@ -75,7 +97,8 @@ const AuthState = props => {
         error: state.error,
         register,
         clearErrors,
-        loadUser
+        loadUser,
+        login
       }}
     >
       {props.children}
